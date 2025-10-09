c4815831a3d65a5cb4ee3250b41dde576ad2d93d
anubhav <anubhavsaxenavibhu@gmail.com>
2025-10-09 21:35:25 +0530

    Update to MySQL database and add mysqlclient to requirements

--- backend/mobile_shop_backend/settings.py
+++ backend/mobile_shop_backend/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'mobile_shop_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mobile',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
