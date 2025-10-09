9c5048e0b63b284d11704b6b29b4dc317b7321e6
anubhav <anubhavsaxenavibhu@gmail.com>
2025-10-09 21:43:28 +0530

    Complete migration and update settings for MySQL database

--- backend/mobile_shop_backend/settings.py
+++ backend/mobile_shop_backend/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mobile',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'anubhav2004',
         'HOST': 'localhost',
         'PORT': '3306',
     }
